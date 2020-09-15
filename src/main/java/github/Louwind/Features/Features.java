69f38a6e0f194603b8c012309ddbaa40bc65797f
Louwind <46799905+Louwind@users.noreply.github.com>
2020-09-15 13:45:55 -0700

    Fabric Environment Setup

---
+++ src/main/java/github/Louwind/Features/Features.java
@@ -0,0 +1,12 @@
+package github.Louwind.Features;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Features implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
