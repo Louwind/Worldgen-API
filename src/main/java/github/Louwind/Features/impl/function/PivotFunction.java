8552da8f7fef5d85d22258f9e234b99d6f3a6518
Louwind <46799905+Louwind@users.noreply.github.com>
2020-10-04 15:27:16 -0700

    Fix: NoSuchElement in PivotFunction
    Caused when "all" it was not defined

--- src/main/java/github/Louwind/Features/impl/function/PivotFunction.java
+++ src/main/java/github/Louwind/Features/impl/function/PivotFunction.java
@@ -1,14 +1,15 @@
 package github.Louwind.Features.impl.function;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import github.Louwind.Features.condition.FeatureCondition;
 import github.Louwind.Features.context.FeatureContext;
+import github.Louwind.Features.context.parameter.FeatureContextParameter;
 import github.Louwind.Features.function.FeatureFunction;
 import github.Louwind.Features.function.FeatureFunctionType;
-import github.Louwind.Features.impl.init.FeatureContextParameters;
 import github.Louwind.Features.impl.init.FeatureFunctions;
 import github.Louwind.Features.util.FeaturesJsonHelper;
 import github.Louwind.Features.util.OptionalBlockPos;
@@ -21,6 +22,9 @@ import net.minecraft.util.math.BlockPos;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static github.Louwind.Features.impl.init.FeatureContextParameters.*;
 
 public class PivotFunction implements FeatureFunction {
 
@@ -45,15 +49,19 @@ public class PivotFunction implements FeatureFunction {
     }
 
     @Override
+    public Set<FeatureContextParameter<?>> getRequiredParameters() {
+        return ImmutableSet.of(PIECES, ROTATION);
+    }
+
+    @Override
     public void accept(PoolStructurePiece poolStructurePiece, FeatureContext context) {
-        List<StructurePiece> pieces = context.get(FeatureContextParameters.PIECES);
-        BlockRotation rotation = context.get(FeatureContextParameters.ROTATION);
+        List<StructurePiece> pieces = context.get(PIECES);
+        BlockRotation rotation = context.get(ROTATION);
 
         OptionalBlockPos optional = this.when.get(rotation);
 
         BlockPos addition = optional.isPresent() ? optional.get(context) : BlockPos.ORIGIN;
-
-        BlockPos pos = this.pos.get(context);
+        BlockPos pos = this.pos.isPresent() ? this.pos.get(context) : BlockPos.ORIGIN;
         BlockPos add = pos.add(addition);
 
         int x = add.getX();
