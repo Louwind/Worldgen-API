222be94425fe277289c2a98b13662d33a7109af0
Louwind <46799905+Louwind@users.noreply.github.com>
2020-10-16 17:22:33 -0700

    Change: Context aware for FeaturesMetadataType

--- src/main/java/github/Louwind/Features/impl/init/FeatureMetadataTypes.java
+++ src/main/java/github/Louwind/Features/impl/init/FeatureMetadataTypes.java
@@ -4,10 +4,12 @@ import github.Louwind.Features.impl.metadata.EntityMetadata;
 import github.Louwind.Features.impl.metadata.BlockstateMetadata;
 import github.Louwind.Features.metadata.FeatureMetadataType;
 
+import static github.Louwind.Features.impl.init.FeatureContextParameters.*;
+
 public class FeatureMetadataTypes {
 
-    public static final FeatureMetadataType ENTITY = new FeatureMetadataType(new EntityMetadata.Serializer());
+    public static final FeatureMetadataType ENTITY = new FeatureMetadataType(new EntityMetadata.Serializer(), builder -> builder.required(BLOCK_INFO, WORLD));
 
-    public static final FeatureMetadataType BLOCKSTATE = new FeatureMetadataType(new BlockstateMetadata.Serializer());
+    public static final FeatureMetadataType BLOCKSTATE = new FeatureMetadataType(new BlockstateMetadata.Serializer(), builder -> builder.required(BLOCK_INFO, WORLD));
 
 }
