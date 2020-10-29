cb45047fd8c14a7f518a43287c6b780b4a3458b5
Louwind <46799905+Louwind@users.noreply.github.com>
2020-10-29 09:45:32 -0700

    Fix: Implement LootBehavior::setLootTable

--- src/main/java/github/Louwind/Features/impl/loot/LootableContainerLootBehavior.java
+++ src/main/java/github/Louwind/Features/impl/loot/LootableContainerLootBehavior.java
@@ -2,6 +2,8 @@ package github.Louwind.Features.impl.loot;
 
 import github.Louwind.Features.loot.LootBehavior;
 import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -11,6 +13,11 @@ public class LootableContainerLootBehavior implements LootBehavior<LootableConta
     @Override
     public void setLootTable(Identifier lootTable, ServerWorld server, LootableContainerBlockEntity blockEntity, BlockPos pos, long seed) {
         blockEntity.setLootTable(lootTable, seed);
+    }
+
+    @Override
+    public void setLootTable(LootTable lootTable, LootContext context, ServerWorld server, LootableContainerBlockEntity blockEntity, BlockPos pos, long seed) {
+
     }
 
 }
