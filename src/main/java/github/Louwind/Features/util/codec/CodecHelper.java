be9afc0af9752c40340b39c6b315938bb66d1c9d
Louwind <mr.louwind@gmail.com>
2021-06-14 10:23:20 -0700

    Fix: Using String for Block Rotation Codec

--- src/main/java/github/Louwind/Features/util/codec/CodecHelper.java
+++ src/main/java/github/Louwind/Features/util/codec/CodecHelper.java
@@ -8,7 +8,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 
 import java.util.Arrays;
-import java.util.function.Supplier;
+import java.util.function.Function;
 import java.util.stream.DoubleStream;
 
 public class CodecHelper {
@@ -19,16 +19,16 @@ public class CodecHelper {
         return state.getEntries().isEmpty() ? Codec.unit(state) : state.codec.fieldOf("properties").codec();
     });
 
-    public static final Codec<BlockRotation> ROTATION = createEnum(BlockRotation::values);
+    public static final Codec<BlockRotation> ROTATION = createEnum(BlockRotation::valueOf);
 
     public static final Codec<Vec3d> VEC3D = PrimitiveCodecHelper.DOUBLE_STREAM.comapFlatMap(stream -> toArray(stream, 3).map(doubles -> new Vec3d(doubles[0], doubles[1], doubles[2])), vec3d -> DoubleStream.of(vec3d.getX(), vec3d.getY(), vec3d.getZ()));
 
-    public static <E extends Enum<?>> Codec<E> createEnum(Supplier<E[]> enums) {
-        return Codec.INT.comapFlatMap(ordinal -> {
-            E value = enums.get()[ordinal];
+    public static <E extends Enum<?>> Codec<E> createEnum(Function<String, E> valueOf) {
+        return Codec.STRING.comapFlatMap(str -> {
+            E value = valueOf.apply(str.toUpperCase());
 
             return DataResult.success(value);
-        }, Enum::ordinal).stable();
+        }, enumValue -> enumValue.name().toLowerCase()).stable();
     }
 
     public static DataResult<double[]> toArray(DoubleStream stream, int length) {
